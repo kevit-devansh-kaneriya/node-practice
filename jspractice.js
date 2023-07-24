fdd1f7e01981c74a0ac10f8cca4fcd30a97b10ab
Devansh Kaneriya <devansh.kaneriya@kevit.io>
2023-07-24 17:38:23 +0530

    js practice

---
+++ jspractice.js
@@ -0,0 +1,32 @@
+var company = "Coding Academy";
+// console.log(company);
+// console.log(company.length)
+// console.log(company.toUpperCase())
+// console.log(company.toLowerCase())
+// console.log(company.substring(0, 6))
+
+// const sentence = 'You cannot end a sentence with because because because is a conjunction';
+// const firstIndex = sentence.indexOf('because');
+// const secondIndex = sentence.indexOf('because', firstIndex + 1);
+// const result = sentence.substr(0, firstIndex) + sentence.substr(secondIndex + 'because'.length);
+// console.log(result);
+
+// console.log(company.includes("world"))
+// console.log(company.split(" "))
+
+// var str1 = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+// console.log(str1.split(","))
+
+// console.log(company.replace("Coding","Microsoft"))
+// console.log(str1.charAt(10))
+// console.log(company.charCodeAt(4))
+// console.log(company.indexOf("n"))
+// console.log(company.lastIndexOf("e"))
+const sentence = '    You cannot end a sentence with because because because is a conjunction   ';
+// console.log(sentence.indexOf("because"))
+// console.log(sentence.lastIndexOf("because"))
+console.log(sentence.search("end"))
+console.log(sentence.trim())
+
+21.
+remaning
